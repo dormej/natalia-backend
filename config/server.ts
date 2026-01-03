08742e5dac701e7090500adce47c595ff051f57a
dormej <dorota.mejlun153@gmail.com>
2026-01-03 19:18:05 +0100

    fix cookies error

--- config/server.ts
+++ config/server.ts
@@ -2,7 +2,9 @@ export default ({ env }) => ({
   host: '0.0.0.0',
   port: env.int('PORT', 1337),
   url: env('PUBLIC_URL', 'https://natalia-backend.onrender.com'),
-  proxy: true,
+  proxy: {
+    koa: true
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
