4c6a863aeaa7f8226eb7faeb952b46ce6a07d38b
dormej <dorota.mejlun153@gmail.com>
2026-01-03 18:28:53 +0100

    fix middlewares cors config

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,21 +1,19 @@
 export default [
   'strapi::errors',
+
   {
     name: 'strapi::cors',
     config: {
-      origin: [
-        'https://natalia-portfolio.onrender.com',
-      ],
+      origin: ['https://natalia-portfolio.onrender.com'],
       credentials: true,
-      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
       headers: ['Content-Type', 'Authorization'],
     },
   },
-  'strapi::logger',
-  'strapi::errors',
+
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
--- config/server.ts
+++ config/server.ts
@@ -1,5 +1,5 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: '0.0.0.0',
   port: env.int('PORT', 1337),
   url: env('PUBLIC_URL', 'https://natalia-backend.onrender.com'),
   proxy: true,
