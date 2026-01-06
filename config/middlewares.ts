24119fb709096f95bf282e16649a1d9b0d5cb9ee
dormej <dorota.mejlun153@gmail.com>
2026-01-06 19:58:38 +0100

    remove changes

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -3,7 +3,7 @@ export default [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:4200', 'https://natalia-portfolio.onrender.com'],
+      origin: ['https://natalia-portfolio.onrender.com'],
       credentials: true,
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
       headers: ['Content-Type', 'Authorization'],
